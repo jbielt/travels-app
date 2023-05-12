b6942ef0ae3a969b5f594975636ff94c27fb652a
jbiel <jbielturon@gmail.com>
2023-05-12 07:19:08 +0200

    doing some jpql queries for FlyRepository

--- src/main/java/com/pim/projects/besttravel/BestTravelApplication.java
+++ src/main/java/com/pim/projects/besttravel/BestTravelApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
 @SpringBootApplication
@@ -14,47 +15,51 @@ import java.util.UUID;
 public class BestTravelApplication implements CommandLineRunner {
 
 
-	private final HotelRepository hotelRepository;
-	private final FlyRepository flyRepository;
-	private final TicketRepository ticketRepository;
-	private final ReservationRepository reservationRepository;
-	private final TourRepository tourRepository;
-	private final CustomerRepository customerRepository;
+    private final HotelRepository hotelRepository;
+    private final FlyRepository flyRepository;
+    private final TicketRepository ticketRepository;
+    private final ReservationRepository reservationRepository;
+    private final TourRepository tourRepository;
+    private final CustomerRepository customerRepository;
 
-	@Autowired
-	public BestTravelApplication(
-			HotelRepository hotelRepository,
-		 	FlyRepository flyRepository,
-	 		TicketRepository ticketRepository,
-		 	ReservationRepository reservationRepository,
-		 	TourRepository tourRepository,
-		 	CustomerRepository customerRepository
-	) {
-		this.hotelRepository = hotelRepository;
-		this.flyRepository = flyRepository;
-		this.ticketRepository = ticketRepository;
-		this.reservationRepository = reservationRepository;
-		this.tourRepository = tourRepository;
-		this.customerRepository = customerRepository;
-	}
+    @Autowired
+    public BestTravelApplication(
+            HotelRepository hotelRepository,
+            FlyRepository flyRepository,
+            TicketRepository ticketRepository,
+            ReservationRepository reservationRepository,
+            TourRepository tourRepository,
+            CustomerRepository customerRepository
+    ) {
+        this.hotelRepository = hotelRepository;
+        this.flyRepository = flyRepository;
+        this.ticketRepository = ticketRepository;
+        this.reservationRepository = reservationRepository;
+        this.tourRepository = tourRepository;
+        this.customerRepository = customerRepository;
+    }
 
 
-	public static void main(String[] args) {
-		SpringApplication.run(BestTravelApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BestTravelApplication.class, args);
+    }
 
-	@Override
-	public void run(String... args) throws Exception {
-		var fly = flyRepository.findById(15L).get(); //.get() porque es un Optional
-		var hotel = hotelRepository.findById(7L).get();
-		var ticket = ticketRepository.findById(UUID.fromString("12345678-1234-5678-2236-567812345678")).get();
-		var reservation = reservationRepository.findById(UUID.fromString("12345678-1234-5678-1234-567812345678")).get();
-		var customer = customerRepository.findById("BBMB771012HMCRR022").get();
+    @Override
+    public void run(String... args) throws Exception {
+//        var fly = flyRepository.findById(15L).get(); //we use .get() because if not returns Optional
+//        var hotel = hotelRepository.findById(7L).get();
+//        var ticket = ticketRepository.findById(UUID.fromString("12345678-1234-5678-2236-567812345678")).get();
+//        var reservation = reservationRepository.findById(UUID.fromString("12345678-1234-5678-1234-567812345678")).get();
+//        var customer = customerRepository.findById("BBMB771012HMCRR022").get();
 
-		log.info(String.valueOf(fly));
-		log.info(String.valueOf(hotel));
-		log.info(String.valueOf(ticket));
-		log.info(String.valueOf(reservation));
-		log.info(String.valueOf(customer));
-	}
+//        log.info(String.valueOf(fly));
+//        log.info(String.valueOf(hotel));
+//        log.info(String.valueOf(ticket));
+//        log.info(String.valueOf(reservation));
+//        log.info(String.valueOf(customer));
+
+        this.flyRepository.findByPriceLessThan(BigDecimal.valueOf(20)).forEach(fly -> System.out.println(fly));
+        this.flyRepository.findByPriceBetween(BigDecimal.valueOf(10), BigDecimal.valueOf(15)).forEach(fly -> System.out.println(fly));
+        this.flyRepository.findByOriginAndDestiny("Grecia", "Mexico").forEach(fly -> System.out.println(fly));
+    }
 }
--- src/main/java/com/pim/projects/besttravel/domain/repository/FlyRepository.java
+++ src/main/java/com/pim/projects/besttravel/domain/repository/FlyRepository.java
@@ -2,8 +2,26 @@ package com.pim.projects.besttravel.domain.repository;
 
 import com.pim.projects.besttravel.domain.entity.Fly;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.math.BigDecimal;
+import java.util.Set;
 
 @Repository
 public interface FlyRepository extends JpaRepository<Fly, Long> {
+
+    //We'll use JPQL for the queries
+
+    @Query("SELECT f FROM fly f WHERE f.price < :price")
+    Set<Fly> findByPriceLessThan(BigDecimal price);
+
+    @Query("SELECT f FROM fly f WHERE f.price BETWEEN :min and :max")
+    Set<Fly> findByPriceBetween(BigDecimal min, BigDecimal max);
+
+    @Query("SELECT f FROM fly f WHERE f.originName = :origin AND f.destinyName = :destiny")
+    Set<Fly> findByOriginAndDestiny(String origin, String destiny);
+
+
+
 }
