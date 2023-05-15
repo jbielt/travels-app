752780c981070efb3306ac537c3367bd446acbcc
jbiel <jbielturon@gmail.com>
2023-05-15 18:06:35 +0200

    validating the DTOs Request

--- src/main/java/com/pim/projects/besttravel/api/model/request/ReservationRequest.java
+++ src/main/java/com/pim/projects/besttravel/api/model/request/ReservationRequest.java
@@ -1,5 +1,6 @@
 package com.pim.projects.besttravel.api.model.request;
 
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,7 +14,20 @@ import java.io.Serializable;
 @Builder
 public class ReservationRequest implements Serializable {
 
+    @Size(min = 18, max = 20, message = "The size must be between 18 and 20 characters")
+    @NotBlank(message = "Id Client is mandatory")
     private String idClient;
+
+    @Positive
+    @NotNull(message = "Id Hotel is mandatory")
     private Long idHotel;
+
+    @Min(value = 1, message = "You have to stay at least one day for your reservation")
+    @Max(value = 30, message = "You can only stay a maximum of 30 days for your reservation")
+    @NotNull(message = "Total days is mandatory")
     private Integer totalDays;
+
+    //@Pattern(regexp = "^(.+)@(.+)$")
+    @Email
+    private String email;
 }
