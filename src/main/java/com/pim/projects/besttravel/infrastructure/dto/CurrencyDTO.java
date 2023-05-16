889aa9318d218da68d5f04c3e5eca5d4f31b3736
jbiel <jbielturon@gmail.com>
2023-05-16 07:24:33 +0200

    spring web client creating properties file and DTO to consume and map currency api

---
+++ src/main/java/com/pim/projects/besttravel/infrastructure/dto/CurrencyDTO.java
@@ -0,0 +1,17 @@
+package com.pim.projects.besttravel.infrastructure.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Currency;
+import java.util.Map;
+
+public class CurrencyDTO implements Serializable {
+
+    @JsonProperty(value = "date")
+    private LocalDate exchangeDate;
+
+    private Map<Currency, BigDecimal> rates;
+}
